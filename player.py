04328f72a2b6d0b0ae94e880a9049d6aa32cb4ba
Enea <eneatontini@gmail.com>
2026-05-24 10:51:46 +0200

    attacco

--- player.py
+++ player.py
@@ -17,25 +17,57 @@ class Player:
         # Stato
         self.on_ground = False
 
+        # Direzione player
+        self.facing_right = True
+
+        # Attacco
+        self.attacking = False
+        self.attack_timer = 0
+
+        self.attack_rect = pygame.Rect(0, 0, 40, 20)
+
     def move(self, keys):
 
         # Movimento orizzontale
         if keys[pygame.K_a]:
             self.rect.x -= self.speed
+            self.facing_right = False
 
         if keys[pygame.K_d]:
             self.rect.x += self.speed
+            self.facing_right = True
 
         # Salto
         if keys[pygame.K_SPACE] and self.on_ground:
             self.vel_y = -self.jump_power
             self.on_ground = False
 
+    def attack(self, keys):
+
+        if keys[pygame.K_j] and not self.attacking:
+
+            self.attacking = True
+            self.attack_timer = 15
+
+        if self.attacking:
+
+            self.attack_timer -= 1
+
+            # Posizione hitbox
+            if self.facing_right:
+                self.attack_rect.x = self.rect.right
+            else:
+                self.attack_rect.x = self.rect.left - 40
+
+            self.attack_rect.y = self.rect.y + 20
+
+            if self.attack_timer <= 0:
+                self.attacking = False
+
     def apply_gravity(self):
 
         self.vel_y += self.gravity
 
-        # Limite caduta
         if self.vel_y > 15:
             self.vel_y = 15
 
@@ -49,7 +81,6 @@ class Player:
 
             if self.rect.colliderect(platform):
 
-                # Collisione dall'alto
                 if self.vel_y > 0:
 
                     self.rect.bottom = platform.top
@@ -60,10 +91,17 @@ class Player:
 
         self.move(keys)
 
+        self.attack(keys)
+
         self.apply_gravity()
 
         self.check_collision(platforms)
 
     def draw(self, screen):
 
+        # Player
         pygame.draw.rect(screen, (220, 220, 220), self.rect)
+
+        # Hitbox attacco
+        if self.attacking:
+            pygame.draw.rect(screen, (255, 80, 80), self.attack_rect)
